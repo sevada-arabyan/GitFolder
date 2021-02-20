80cae5d478ed70e9c3570b0e4cab853e8c93ebe6
Sevada <neoarpa@gmail.com>
2021-02-20 19:07:36 +0300

    The code for Vector and List is added into the project folder.

---
+++ learningVectorList.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <random>
+#include <iterator>
+#include <cassert>
+
+using std::cout;
+using std::endl;
+
+using std::vector;
+using std::list;
+
+void generateVector(vector<int>& vec, const size_t size){
+	assert(size > 0);
+	//size_t size{vec.size()};
+	std::default_random_engine engine;
+	engine.seed(1337);
+	std::uniform_int_distribution<int> distro(0, 15);
+	for(size_t i{0}; i < size; ++i){
+		vec.push_back(distro(engine));
+	}
+}
+void printVector(const vector<int>& vec){
+	for(const auto& i : vec){
+		cout << i << ' ';
+	}
+	cout << endl;
+}
+int main(){
+	vector<int> vec;
+	size_t size{10};
+	vec.reserve(size);
+//	cout << "Hello" << endl;
+	generateVector(vec, size);
+	printVector(vec);
+
+}
